86e181aa14d31a9a1fa357f689764880454f82d5
Dmitriy36 <dmitriy_xix@hotmail.com>
2025-12-12 08:59:23 -0500

    hardcoded for testing establish conn

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -1,8 +1,8 @@
 const config = {
   user: "admin",
-  password: "your_password",
-  server: "localhost",
-  database: "your_database_name",
+  password: "g-Y~aPz8-i*Mk~O~M2*j]LkA554C",
+  server: "integrated-apar-apat.c1vwa9fou9fe.us-east-2.rds.amazonaws.com",
+  database: "integrated-apar-apat",
   port: 1433,
   // options: {
   //     encrypt: false,
