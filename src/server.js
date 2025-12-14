200efb904e5b28ff64d4f7ad0a169d9776217604
Dmitriy36 <dmitriy_xix@hotmail.com>
2025-12-13 20:10:20 -0500

    stiLl

--- src/server.js
+++ src/server.js
@@ -11,26 +11,26 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const config = {
-    server: "integrated-apar-apat.c1vwa9fou9fe.us-east-2.rds.amazonaws.com",
-    database: "Integrated_APAR",
-    user: "admin",
-    password: "g-Y~aPz8-i*Mk~O~M2*j]LkA554C",
-    port: 1433,
-    options: {
-        encrypt: true, // Use encryption for AWS RDS
-        trustServerCertificate: false,
-     }
-};
+// const config = {
+//     server: "integrated-apar-apat.c1vwa9fou9fe.us-east-2.rds.amazonaws.com",
+//     database: "Integrated_APAR",
+//     user: "admin",
+//     password: "g-Y~aPz8-i*Mk~O~M2*j]LkA554C",
+//     port: 1433,
+//     options: {
+//         encrypt: true, // Use encryption for AWS RDS
+//         trustServerCertificate: false,
+//      }
+// };
 
-app.get("/api/dbTest", (req, res) => {  
-res.json(connResult + ", " +connErr)
-});
+// app.get("/api/dbTest", (req, res) => {  
+// res.json(connResult + ", " +connErr)
+// });
 
-app.get("/api/nondbTest", (req, res) => {  
-const testString = "I can see.";
-res.json(testString)
-});
+// app.get("/api/nondbTest", (req, res) => {  
+// const testString = "I can see.";
+// res.json(testString)
+// });
 
 app.get("/api/users", (req, res) => {
   const users = [
