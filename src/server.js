ed3e80ca78303675767d36fd8c62e0bdd16a9f25
Dmitriy36 <dmitriy_xix@hotmail.com>
2025-12-23 15:01:52 -0500

    stiLl

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ app.post("/byregion", async (req, res) => {
     const request = pool.request();
 
     const result = await request.query(
-      `select distinct externalid from [Meta].[Facilities_Geo] where region=${region}`
+      `select ExternalId, FacilityName, Longitude, Latitude, VaVisnNumber, Region, TimeZoneId from [Meta].[Facilities_Geo] where region=${region}`
     );
     res.json({ data: result.recordset }); // Returns query results
   } catch (err) {
